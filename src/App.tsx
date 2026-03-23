7e506e448742f9db56f4e0fe1cf5d201f32d45a5
agsaputrawork-maker <agsaputra.work@gmail.com>
2026-03-24 03:25:27 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Layout Components (Global)
 import Navbar from './components/Navbar';
 import ScrollProgress from './components/ScrollProgress';
 import Background from './components/Background';
 import Footer from './components/Footer';
-
-// Pages
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
@@ -15,23 +11,15 @@ function App() {
   return (
     <Router>
       <div className="font-sans text-slate-400 bg-space-900 overflow-x-hidden antialiased selection:bg-cyan-500/30 selection:text-white min-h-screen flex flex-col">
-        {/* Komponen Global (Akan muncul di semua halaman) */}
         <ScrollProgress />
         <Background />
         <Navbar />
-
-        {/* Routing Logic */}
         <main className="flex-grow">
           <Routes>
-            {/* Route Utama */}
             <Route path="/" element={<Home />} />
-            
-            {/* Route 404 (Menangkap semua URL yang tidak terdaftar) */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-
-        {/* Footer Global */}
         <Footer />
       </div>
     </Router>
