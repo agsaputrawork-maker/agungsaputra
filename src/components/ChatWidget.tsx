36ba4d080747572ca4f09695611339d174f6ef3e
agsaputrawork-maker <agsaputra.work@gmail.com>
2026-03-24 14:48:10 +0700

    Update ChatWidget.tsx

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -24,7 +24,7 @@ const ChatWidget = () => {
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const API_URL = "https://script.google.com/macros/s/AKfycbzKjSfC6MV_bni4x44KN6_-LvDLGvUsjC1FKurKH6H2BlyxbUiinxDR_9ndQYfuS4oa/exec";
+  const API_URL = "https://script.google.com/macros/s/AKfycbxI5X8coJ9c2P_m0dVi6qOetwPAfIw7LLuVsw5C7Kt5yZoAsI-rhk9HO50QxZbQg4uj/exec";
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
